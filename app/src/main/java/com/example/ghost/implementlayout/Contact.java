8aae3afe02dd48a84999e3faf02920035db5ad21
razhou <r4ju.riy4nd4@gmail.com>
2016-02-23 15:42:15 +0700

    IMPLEMENTLAYOUT (TASK1)

---
+++ app/src/main/java/com/example/ghost/implementlayout/Contact.java
@@ -0,0 +1,16 @@
+package com.example.ghost.implementlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Contact extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_contact);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/ghost/implementlayout/Home.java
@@ -0,0 +1,17 @@
+package com.example.ghost.implementlayout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+    }
+}
